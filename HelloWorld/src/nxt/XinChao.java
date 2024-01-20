5b71c0c1a95f1cd90b002712a6ba4bdf1ec56bb8
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-01-20 11:47:13 +0700

    Hello

---
+++ HelloWorld/src/nxt/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package nxt;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quẩn lý mã nguồn GIT");
+	}
+
+}
