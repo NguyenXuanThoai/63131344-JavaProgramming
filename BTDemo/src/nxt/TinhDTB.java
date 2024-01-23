74ae5aedc1b9cb723542e0f7075212e9cc6b9b1f
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-01-23 10:47:21 +0700

    Create TinhDTB.java

---
+++ BTDemo/src/nxt/TinhDTB.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package nxt;
+/**
+ * 
+ */
+public class TinhDTB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// tính điểm trung bình 
+		double a = 8.1;
+		double b = 8.3;
+		double c = 8.5;	
+		double dtb = (a * 2 + b + c)/4;
+		System.out.printf("Điểm trung bình: %.2f\n", dtb);
+	}
+}
