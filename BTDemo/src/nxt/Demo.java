e65fff804e4e41fabc249c37439661dbc3f8f469
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-01-23 10:32:00 +0700

    BTDemo

---
+++ BTDemo/src/nxt/Demo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package nxt;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// cộng 2 số nguyên
+		int a = 10, b=5, c;
+		c = a + b;
+		System.out.print(a+ "+" +b+ "=" +c);
+			
+	}
+
+}
---
+++ PhepToan/src/nxt/Cong.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package nxt;
+
+/**
+ * 
+ */
+public class Cong {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b, c;
+		
+	}
+
+}
