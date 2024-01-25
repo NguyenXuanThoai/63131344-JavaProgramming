34c29728b37cc07cddee81fce3cc41a3104eab1f
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-01-25 16:54:54 +0700

    Update Bai3.java

--- THJavaCoBan/src/Lab1/Bai3.java
+++ THJavaCoBan/src/Lab1/Bai3.java
@@ -1,5 +1,14 @@
 package Lab1;
-
+import java.util.Scanner;
 public class Bai3 {
+	public static void main(String[] args) {
+		// Tính và xuất thể tích của khối lập phương
 
+	Scanner sc = new Scanner(System.in);
+	double canh,theTich;
+	System.out.print("Nhập vào cạnh của khối lập phương: ");
+	canh = sc.nextDouble();
+	theTich = Math.pow(canh, 3);
+	System.out.printf("Thể tích của khối lập phương là: %.2f",theTich);
+	}
 }
