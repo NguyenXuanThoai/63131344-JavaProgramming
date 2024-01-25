8de26a97beaab954f765976aae7b9684dbb6ad48
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-01-25 17:24:14 +0700

    Update Bai1.java

--- THJavaCoBan/src/Lab2/Bai1.java
+++ THJavaCoBan/src/Lab2/Bai1.java
@@ -1,10 +1,25 @@
 package Lab2;
-
+import java.util.Scanner;
 public class Bai1 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		// Viết chương trình cho phép giải phương trình bậc nhất trong đó các hệ số a và b nhập từ bàn phím.
+		
+        Scanner sc = new Scanner(System.in);
+        double a,b;
+        System.out.print("Nhập hệ số a: ");
+        a = sc.nextDouble();
+        System.out.print("Nhập hệ số b: ");
+        b = sc.nextDouble();
+        double x = (double) (-b / a);
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình có vô số nghiệm.");
+            } else {
+                System.out.println("Phương trình vô nghiệm.");
+            }
+        } else {
+            System.out.printf("Nghiệm của phương trình là: %.2f ",x);
+        }
+    }
 }
