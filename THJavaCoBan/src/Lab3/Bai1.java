fd16e503dbeed394962b9ff344a5a28489cc5b65
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-02-22 22:43:42 +0700

    Update Bai1.java
    Hoàn thành bài 1

--- THJavaCoBan/src/Lab3/Bai1.java
+++ THJavaCoBan/src/Lab3/Bai1.java
@@ -2,7 +2,7 @@
  * 
  */
 package Lab3;
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -13,6 +13,26 @@ public class Bai1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Nhập vào số N : ");
+		int n = sc.nextInt();
+		boolean SNT = true;
+		if ( n <= 1) {
+			SNT = false;
+		} else {
+			for(int i=2; i <= Math.sqrt(n); i++){ 
+				if(n % i == 0){
+					SNT = false;
+					break;
+				}
+			}
+		}
+		
+		if (SNT) {
+	            System.out.println(n + " là số nguyên tố.");
+		} else {
+	            System.out.println(n + " không phải là số nguyên tố.");
+	      }	
 		
 	}
 
